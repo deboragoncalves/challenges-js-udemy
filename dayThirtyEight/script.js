bfade58c060c97609416700f6d4327c76bc1df35
Debora Goncalves <deboracristinapereira123@gmail.com>
2023-08-02 00:02:20 -0300

    Exibir modal, fechando após 1min - Day 38 - Udemy JS

---
+++ dayThirtyEight/script.js
@@ -0,0 +1,53 @@
+window.onload = () => {
+    createModal();
+}
+
+const createModal = () => {
+    let containerModal = document.createElement("div");
+    containerModal.id = "modal";
+    containerModal.classList.add("modal");
+
+    let subcontainerModal = document.createElement("div");
+    subcontainerModal.classList.add("modal-content");
+
+    let buttonClose = document.createElement("img");
+    buttonClose.src = "close.svg";
+    buttonClose.alt = "Fechar modal";
+    buttonClose.style.width = "16px";
+    buttonClose.style.height = "16px";
+    buttonClose.style.cursor = "pointer";
+    buttonClose.style.alignSelf = "flex-end";
+    buttonClose.style.marginBottom = "15px";
+
+    buttonClose.addEventListener("click", () => {
+        let modal = document.querySelector("#modal");
+        modal.style.display = "none";
+    })
+
+    let textModal = document.createElement("p");
+    textModal.textContent = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. In at urna faucibus, tristique justo eu, convallis lacus. Integer fringilla porta ante, in ornare augue consectetur id. Curabitur vel leo erat. Sed vel tellus et eros euismod feugiat eget nec orci. Nunc hendrerit scelerisque leo, rutrum rhoncus lorem hendrerit dapibus. Nunc fringilla arcu eget tellus tempus, a placerat arcu viverra. Fusce aliquet auctor nibh sit amet pretium. Suspendisse nec augue pharetra, malesuada tortor ut, cursus enim.";
+
+    subcontainerModal.appendChild(buttonClose);
+    subcontainerModal.appendChild(textModal);
+
+    containerModal.appendChild(subcontainerModal);
+
+    let buttonShowModal = document.createElement("button");
+    buttonShowModal.textContent = "Exibir modal";
+    buttonShowModal.classList.add("btn");
+    buttonShowModal.classList.add("btn-primary");
+    buttonShowModal.addEventListener("click", () => {
+        let modal = document.querySelector("#modal");
+        if (!modal) return;
+        modal.style.display = "block";
+
+        // depois de 1min fechar modal aberto
+        setTimeout(() => {
+            modal.style.display = "none";
+        }, 60000);
+    });
+
+    let bodyHtml = document.querySelector("body");
+    bodyHtml.appendChild(buttonShowModal);
+    bodyHtml.appendChild(containerModal);
+}
